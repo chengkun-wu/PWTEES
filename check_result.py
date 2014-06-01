c02188e203f51d284d2705af31222bd3f1dac540
chengkun_wu <wchknudt@gmail.com>
2014-06-01 22:32:02 +0100

    Print out PMIDs that are not properly processed

--- check_result.py
+++ check_result.py
@@ -2,22 +2,29 @@ import sys, os
 from os import walk
 from os.path import expanduser
 
-home = expanduser("~")
+#home = expanduser("~")
 f = []
 corpus = []
-opath = "%s/scratch/PWTEES/bo/*-events.tar.gz" % home
-ipath = "%s/scratch/PWTEES/bi/" % home 
+opath = "bo/*-events.tar.gz"
+ipath = "bi/*.txt"
 out = os.popen("ls " + opath)
 
 for file in out.readlines():
-	f.append(os.path.basename(file.strip().replace("-events.tar.gz", "")))
+	f.append(os.path.basename(file.strip().replace("-events.tar.gz", "")).replace(".txt", ""))
 
 out = os.popen("ls " + ipath)
 
 for file in out.readlines():
-	corpus.append(os.path.basename(file.strip()))
+	corpus.append(os.path.basename(file.strip()).replace(".txt", ""))
 
-print set(corpus) - (set(f))
 
-print len(f)
-print len(corpus)
+#print len(f)
+#print len(corpus)
+
+if len(f) == len(corpus):
+	print >> sys.stderr, 'All input files have been successfully processed!'
+else:
+	print >> sys.stderr, 'Failed to generate events for some of the files!'
+	
+	for pmid in set(corpus) - (set(f)):  
+		print pmid	
